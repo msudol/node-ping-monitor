0a6f9a0f25d6bce799d3a7ac632a5656c39bb8d9
msudol <msudol@anaren.com>
2018-05-01 15:24:22 -0400

    initial commit just basic use of tcpie

---
+++ README.md
@@ -0,0 +1,5 @@
+# Server Mon
+
+The server monitor application will monitor servers and potentially alert on them.  
+
+It should do ping, http, and possibly advanced metrics when available like prometheus.
---
+++ app.js
@@ -0,0 +1,18 @@
+var tcpie = require('tcpie');
+var pie = tcpie('johnson.anaren.com', 10000, {count: 4, interval: 500, timeout: 2000});
+ 
+pie.on('connect', function(stats) {
+  console.info('connect', stats);
+}).on('error', function(err, stats) {
+  console.error(err, stats);
+}).on('timeout', function(stats) {
+  console.info('timeout', stats);
+}).on('end', function(stats) {
+  console.info(stats);
+  // -> {
+  // ->   sent: 10,
+  // ->   success: 10,
+  // ->   failed: 0,
+  // ->   target: { host: 'google.com', port: 80 }
+  // -> }
+}).start()
