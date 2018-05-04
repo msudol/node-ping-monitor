1e46e995ddc0a320b6f6f4554b739d0270d29bc7
msudol <msudol@anaren.com>
2018-05-04 12:28:20 -0400

    making better output

--- app.js
+++ app.js
@@ -3,5 +3,7 @@
 var Ping = require('./Ping.js');
 
 var p1 = new Ping('google.com', 80, 4, 500, 2000);
+var p2 = new Ping('bing.com', 80, 4, 500, 2000);
 
 p1.init();
+p2.init();
--- ping.js
+++ ping.js
@@ -1,11 +1,15 @@
 // ping.js
 "use strict"
 
+// use the tcpie module
 var tcpie = require('tcpie');
 
+// create a ping class
 class Ping{
      
+    // our constructor function fires on new Ping(etc)
 	constructor(address, port, count, interval, timeout) {
+        var self = this;
 		this.address = address;
 		this.port = port;
         this.settings = {
@@ -13,21 +17,42 @@ class Ping{
             interval: interval,
             timeout: timeout
         };
+        // maintain array of round trip times
+        this.rttArray = [];
+        this.rttAvg = null;
+        
+        // create an instance of tcpie for this ping
         this.pie = tcpie(this.address, this.port, this.settings);
-        this.pie.on('connect', function(stats) {
-          console.info('connect', stats);
+        
+        // setup event handlers
+        this.pie.on('connect', function(stats) {         
+            console.info('connected to: ' + stats.target.host + ':' + stats.target.port + ' seq=' + stats.sent + ' time=' + stats.rtt );
+            // push time into the array
+            self.rttArray.push(stats.rtt);
         }).on('error', function(err, stats) {
-          console.error(err, stats);
+            console.error(err, stats);
         }).on('timeout', function(stats) {
-          console.info('timeout', stats);
+            console.info('timeout', stats);
         }).on('end', function(stats) {
-          console.info(stats);
-        })        
+            self.rttAvg = self.rttArray.reduce((total, amount, index, array) => {
+                total += amount;
+                if(index === array.length-1) { 
+                    return total/array.length;
+                } else { 
+                    return total;
+                }
+            });
+            
+            console.info('finished: ' + stats.target.host + ':' + stats.target.port + ' success=' + stats.success + ' failed=' + stats.failed +  ' avg=' + self.rttAvg );
+        })  
+        
 	}
 
+    // init function runs start.
     init() {
         this.pie.start();
     }
+    
         
 }
 	
