ebb205e1188dab0c4a5ce261ac51ae770050fa22
msudol <msudol@anaren.com>
2018-05-08 16:24:33 -0400

    got the targets running on nedb

--- app.js
+++ app.js
@@ -3,7 +3,12 @@
 
 var Datastore = require('nedb');
 var db = {};
-db.targets = new Datastore({filename: 'db/targets.db', autoload: true});
+db.hosts = new Datastore({filename: 'db/targets.db', autoload: true});
+
+// Using a unique constraint with the index
+db.hosts.ensureIndex({ fieldName: 'host', unique: true }, function (err) {
+    //console.error(err);
+});
 
 var Ping = require('./ping.js');
 var Runner = require('./runner.js');
@@ -19,24 +24,53 @@ var targets = [
 var setupTarget = function(t) {
     
     // coming soon - nedb storage of results
-    db.targets.find({host: targets[t].host}, function(err, docs) {
-        console.log(docs.length);
+    db.hosts.find({host: targets[t].host}, function(err, docs) {
+        
+        // no docs
+        if (docs.length == 0) {
+            console.log("No NEDB document found for: " + targets[t].host + " - creating one!");
+            
+            db.hosts.insert(targets[t], function (err, newDoc) {   
+                // Callback is optional
+                // newDoc is the newly inserted document
+                setupPing(newDoc);
+            });
+            
+        }
+        // found 1 doc, which is all we want
+        else if (docs.length == 1) {
+            console.log("NEDB document found for: " + targets[t].host);
+            
+            setupPing(docs[0]);
+            
+        }
+        // something else
+        else {
+            console.log("Something went wrong looking for document for: " + targets[t].host);
+            
+        }
+        
     });
     
+};
+
+// running the ping setup
+var setupPing = function(p) {
+ 
     // setup the ping 
-    targets[t].ping = new Ping(targets[t]);
+    p.ping = new Ping(p);
     
     // setup the runner
-    targets[t].runner = new Runner(targets[t].cron, function() {    
+    p.runner = new Runner(p.cron, function() {    
         
         // fire the ping on the runner timer
         //targets[t].ping.init();  - doesn't work?
         
         // have to create new instance of tcpie for now.
-        targets[t].ping = new Ping(targets[t]);
+        p.ping = new Ping(p);
                 
-    });
-};
+    }); 
+}
 
 // loop through targets and set them up
 for (var t = 0; t < targets.length; t++) { 
